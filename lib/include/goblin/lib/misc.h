502a3093f4b3fef555f56ee23552133f41699aa4
Jasper <jasper.r.post@gmail.com>
2026-01-07 09:16:31 +0100

    fixed some issues that prevented compilation on the server

--- lib/include/goblin/lib/misc.h
+++ lib/include/goblin/lib/misc.h
@@ -5,6 +5,7 @@
 
 #include <format>
 #include <Eigen/Dense>
+#include <vector>
 
 template<>
 struct std::formatter<Eigen::VectorXd, char> {
@@ -16,6 +17,25 @@ struct std::formatter<Eigen::VectorXd, char> {
         auto out = ctx.out();
         *out++ = '[';
         for (int i = 0; i < v.size(); ++i) {
+            out = std::format_to(out, "{}", v[i]);
+            if (i + 1 < v.size())
+                out = std::format_to(out, ", ");
+        }
+        *out++ = ']';
+        return out;
+    }
+};
+
+template<typename T, typename Alloc>
+struct std::formatter<std::vector<T, Alloc>, char> {
+    constexpr auto parse(std::format_parse_context& ctx) {
+        return ctx.begin();
+    }
+
+    auto format(const std::vector<T, Alloc>& v, std::format_context& ctx) const {
+        auto out = ctx.out();
+        *out++ = '[';
+        for (std::size_t i = 0; i < v.size(); ++i) {
             out = std::format_to(out, "{}", v[i]);
             if (i + 1 < v.size())
                 out = std::format_to(out, ", ");
