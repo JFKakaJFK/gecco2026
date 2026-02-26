7c72a2bb80e19e56c6a3d4d35acd959f877ca489
Jasper <jasper.r.post@gmail.com>
2026-02-26 17:01:29 +0100

    added experiments for pyoperon

---
+++ experiments/python/pyoperon/src/run.py
@@ -0,0 +1,140 @@
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import csv
+import os
+from collections.abc import Callable
+from pathlib import Path
+from typing import Never
+
+import numpy as np
+from pyoperon.sklearn import SymbolicRegressor
+from tqdm import tqdm
+
+from src.experiment_config import OPERATOR_SETS
+from src.task import Task, TaskGenerator
+
+JobQueue = list[tuple[Callable[[Task, Path], None], list[Never], dict[str, Task]]]
+
+
+def run_one_task(task: Task) -> dict:
+    X_train = np.load(task["X_path"].absolute())
+    y_train = np.load(task["y_path"].absolute())
+
+    obs = task["num_observations"]
+    feat = task["num_features"]
+
+    assert X_train.shape[0] >= obs, (
+        f"Requested obs={obs}, but fold has only {X_train.shape[0]}"
+    )
+    assert X_train.shape[1] >= feat, (
+        f"Requested feat={feat}, but fold has only {X_train.shape[1]}"
+    )
+
+    X_train = X_train[:obs, :feat]
+    y_train = y_train[:obs].reshape(-1)
+
+    temp = float(np.nanmax(np.abs(y_train)))
+    const_range = (-temp, temp)
+
+    if task["population_size"] is None:
+        raise ValueError("Population size cannot be None")
+
+    reg = SymbolicRegressor(
+        allowed_symbols="constant,variable,add,sub,mul,div,sin,cos,square,sqrt,exp,log,pow",
+        crossover_probability=1.0,
+        crossover_internal_probability=0.9,
+        mutation_probability=0.1,
+        objectives=["mse"],
+        max_length=task["max_length"],
+        max_depth=task["depth"],
+        initialization_max_length=task["max_length"],
+        initialization_max_depth=task["depth"],
+        population_size=task["population_size"],
+        generations=1_000_000_000,
+        max_evaluations=1_000_000_000,
+        tournament_size=20,
+        max_time=3600,  # assumed to be seconds
+        random_state=task["seed"],
+    )
+
+    reg.fit(X_train, y_train)
+    best = min(reg.pareto_front_, key=lambda s: s["objective_values"][0])
+    mse = best["objective_values"][0]
+    tree = reg.get_model_string(best["tree"], 12)
+
+    return {
+        "dataset": task["dataset"],
+        "population_size": task["population_size"],
+        "num_observations": task["num_observations"],
+        "num_features": task["num_features"],
+        "depth": task["depth"],
+        "operator_set": task["operator_set"],
+        "fold": task["fold"],
+        "iteration": task["iteration"],
+        "seed": task["seed"],
+        "mse": float(mse),
+        "tree": f"'{tree}'",
+    }
+
+
+def run_cpu_tasks(
+    tasks: TaskGenerator,
+    output_directory: Path,
+    max_workers: int | None = None,
+    dry_run: bool = False,
+) -> None:
+    if not dry_run:
+        os.makedirs(output_directory, exist_ok=True)
+
+    results_path = output_directory / "results.csv"
+    fieldnames: list[str] = [
+        "dataset",
+        "population_size",
+        "num_observations",
+        "num_features",
+        "depth",
+        "operator_set",
+        "fold",
+        "iteration",
+        "seed",
+        "mse",
+        "tree",
+    ]
+
+    jobs: JobQueue = []
+
+    for task in tasks:
+        jobs.append((run_one_task, [], {"task": task}))
+
+    n_jobs = len(jobs)
+
+    if dry_run:
+        print(f"Total GPU tasks: {n_jobs}")
+        return
+
+    with open(results_path, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        with ProcessPoolExecutor(max_workers=max_workers) as pool:
+            futures = {pool.submit(fn, *args, **kwargs) for fn, args, kwargs in jobs}
+
+            progress = tqdm(total=n_jobs, leave=False, ascii=True)
+
+            try:
+                for f in as_completed(futures):
+                    try:
+                        result = f.result()
+                        writer.writerow(result)
+
+                    except Exception as e:
+                        print(e)
+
+                    finally:
+                        progress.update()
+
+            except KeyboardInterrupt:
+                pool.shutdown(wait=False, cancel_futures=True)
+
+                progress.close()
+
+                return
